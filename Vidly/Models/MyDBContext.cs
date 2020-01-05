c5b98d45b539edbbda9c394a5b78574d49833ca2
SERGIO ESPINOZA <SERGIO ESPINOZA@DESKTOP-QKDAGMV>
2020-01-04 23:33:11 -0600

    46. Exercise

--- Vidly/Models/MyDBContext.cs
+++ Vidly/Models/MyDBContext.cs
@@ -11,5 +11,6 @@ namespace Vidly.Models
         }
         public DbSet<Customer> Customers { get; set; } // My domain models
         public DbSet<Movie> Movies { get; set; }// My domain models
+        public DbSet<MembershipType> MembershipTypes { get; set; }// My domain models
     }
 }
